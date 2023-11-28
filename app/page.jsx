62032bba01e63914a320e6122dd9e89328bcc095
Logan Hobbs <lhobbs@cti.local>
2023-11-28 11:31:00 -0600

    started connecting to mongoose

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,23 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  console.log(process.env.MONGO)
+  console.log('TESTING')
   return (
-    <main className={styles.main}>
-      <div className={styles.mainContainer}>
-        <div className={styles.heroText}>
-          <h2 className={styles.tag}>Always</h2>
-          <h2 className={styles.tag}>Thinking</h2>
-          <h2 className={styles.tag}>Productivity.</h2>
-          <p className={styles.subTag}>Since 1969</p>
-        </div>
+    <main>
+      <div className={styles.heroWrap}>
+        <section className={styles.hero}>
+          <div className={styles.heroText}>
+            <h2 className={styles.tag}>Always</h2>
+            <h2 className={styles.tag}>Thinking</h2>
+            <h2 className={styles.tag}>Productivity.</h2>
+            <p className={styles.subTag}>Since 1969</p>
+          </div>
+        </section>
       </div>
+      <section className={styles.articles}>
+        <h1 className={styles.articlesHeader}>Articles</h1>
+      </section>
     </main>
   )
 }
