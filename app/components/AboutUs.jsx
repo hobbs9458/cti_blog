1f9682a5244a19728e8098688547dfe2426d22d5
hobbs9458 <hobbs9458@gmail.com>
2023-12-12 21:19:59 -0600

    using fill technique of images

--- app/components/AboutUs.jsx
+++ app/components/AboutUs.jsx
@@ -1,24 +1,20 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-import styles from "./AboutUs.module.css";
-import pieChartImage815 from "../../public/cost_pie_chart_815.svg";
-import pieChartImage600 from "../../public/cost_pie_chart_600.svg";
-import pieChartImage350 from "../../public/cost_pie_chart_350.svg";
-import pieChartImage337 from "../../public/cost_pie_chart_337.svg";
+import styles from './AboutUs.module.css';
 
-import metalTool815 from "../../public/metal_tool.jpg";
-import metalTool600 from "../../public/metal_tool.jpg";
+import pieChartImage815 from '../../public/cost_pie_chart_815.svg';
+import metalTool815 from '../../public/metal_tool815.jpg';
 
 function AboutUs() {
   return (
     <div className={styles.aboutUsWrap}>
       <h1 className={`${styles.aboutUsH1}`}>About Us</h1>
       <p className={styles.aboutUsP}>
-        Founded in 1969 under the motto of "Always Thinking Productivity",
-        Cutting Tools Inc. is the premier distributor and machine solutions
-        provider for the Automotive, Aerospace, Defense, Energy, Firearms, and
-        Medical manufacturing industries.
+        Founded in 1969 under the motto of &quot;Always Thinking
+        Productivity&quot;, Cutting Tools Inc. is the premier distributor and
+        machine solutions provider for the Automotive, Aerospace, Defense,
+        Energy, Firearms, and Medical manufacturing industries.
       </p>
       <p className={styles.aboutUsP}>
         After more than 50 years of experience and proven excellence, we are
@@ -29,13 +25,13 @@ function AboutUs() {
       </p>
       <p className={styles.aboutUsP}>
         We are an engineering driven, machine solutions provider and we solve
-        our customers' problems at the spindle, saving time and money while
+        our customer&apos; problems at the spindle, saving time and money while
         making them more efficient and profitable.Our sales team is comprised of
         degreed engineers and those with decades of experience in the
         manufacturing industry, and we know that sets us apart.
       </p>
       <h2 className={`${styles.aboutUsH2}`}>
-        Our Customers' Success Is Our Priority
+        Our Customers&apos; Success Is Our Priority
       </h2>
       <p className={styles.aboutUsP}>
         Though tooling costs only account for 3&#37; of production cost, the
@@ -45,57 +41,40 @@ function AboutUs() {
         technical tooling competency has helped customers double and even triple
         their productivity while saving them time and money.
       </p>
-      <Image
-        src={pieChartImage815}
-        width={815}
-        height={519}
-        className={styles.pieImg815}
-      />
-      <Image
-        src={pieChartImage600}
-        width={600}
-        height={382}
-        className={styles.pieImg600}
-      />
-      <Image
-        src={pieChartImage350}
-        width={350}
-        height={223}
-        className={styles.pieImg350}
-      />
-      <Image
-        src={pieChartImage337}
-        width={337}
-        height={215}
-        className={styles.pieImg337}
-      />
-      <Link href="/what-we-do" className="link">
+      <div className={styles.imgWrap}>
+        <Image
+          src={pieChartImage815}
+          fill
+          className={styles.pieImg815}
+          alt='pie chart showing only 3% of cost is on tooling'
+          sizes='(max-width: 460px) 337px,(max-width: 687px) 400px,(max-width: 897px) 600px, 815px'
+        />
+      </div>
+      <Link href='/what-we-do' className='link'>
         <h2 className={`${styles.aboutUsH2}`}>What We Do</h2>
       </Link>
       <p className={styles.aboutUsP}>
         From turn-key tool setup and CNC programming to coolant management and
         engineering consulting, we&#39;ve got your machining needs covered. Our
         expertise extends to tool reconditioning, ball screw repair, gaging,
-        metrology, MRO, and inventory management. Click{" "}
-        <Link href="/what-we-do" className="link-underline link">
+        metrology, MRO, and inventory management. Click{' '}
+        <Link href='/what-we-do' className='link-underline link'>
           here
-        </Link>{" "}
-        for more details on the services we provide and{" "}
-        <Link href="#contact" className="link-underline link">
+        </Link>{' '}
+        for more details on the services we provide and{' '}
+        <Link href='#contact' className='link-underline link'>
           contact Cutting Tools Inc.
-        </Link>{" "}
+        </Link>{' '}
         to see how we can optimize your operations.
       </p>
-      <Image
-        src={metalTool815}
-        alt="metal tool"
-        className={styles.metalTool815}
-      />
-      <Image
-        src={metalTool600}
-        alt="metal tool"
-        className={styles.metalTool815}
-      />
+      <div className={styles.imgWrap}>
+        <Image
+          src={metalTool815}
+          fill
+          alt='metal tool'
+          sizes='(max-width: 460px) 337px,(max-width: 687px) 400px,(max-width: 897px) 600px, 815px'
+        />
+      </div>
     </div>
   );
 }
