2b38b7e41e9a09e6fa16f053e913b7d9e48cff0f
Logan Hobbs <lhobbs@cti.local>
2023-12-20 13:59:27 -0600

    added access page in place of login and logout page

--- app/(auth)/access/page.jsx
+++ app/(auth)/access/page.jsx
@@ -6,16 +6,35 @@ import { useRouter } from "next/navigation";
 import styles from "./access.module.css";
 import AuthForm from "../../components/AuthForm";
 
+import { Hourglass } from "react-loader-spinner";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import LogoutBtn from "../../components/LogoutBtn";
 
 const supabase = createClientComponentClient();
 
 export default function Login() {
   const [error, setError] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const router = useRouter();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function checkSession() {
+      setLoading(true);
+      const session = (await supabase.auth.getSession()).data.session;
+      console.log(session);
+      if (session) {
+        console.log("logged in");
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+      setLoading(false);
+    }
+
+    checkSession();
+  }, []);
 
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
@@ -35,10 +54,31 @@ export default function Login() {
     }
   };
 
+  if (loading) {
+    return (
+      <div style={{ minHeight: "650px" }}>
+        <Hourglass
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="hourglass-loading"
+          wrapperStyle={{ display: "block", margin: "8rem auto 0" }}
+          wrapperClass=""
+          colors={["#1b1b1b", "#1b1b1b"]}
+        />
+      </div>
+    );
+  }
+
   return (
     <main className={styles.access}>
-      <h2 className="text-center">Login</h2>
-      <AuthForm handleSubmit={handleSubmit} />
+      {loggedIn ? (
+        <h2 className="text-center">You are logged in</h2>
+      ) : (
+        <h2 className="text-center">Login</h2>
+      )}
+
+      {loggedIn ? <LogoutBtn /> : <AuthForm handleSubmit={handleSubmit} />}
       {error && <div className="error">{error}</div>}
     </main>
   );
